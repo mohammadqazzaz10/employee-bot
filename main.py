acb974dc61d5449d3c1c400f0a8cea79983523d7
mohammadqazzaz10 <mohammadqazzaz10@gmail.com>
2025-11-03 20:09:11 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from employee_management_bot.bot import run_bot
-from employee_management_bot.db import initialize_database_tables
-
-# عند تشغيل هذا الملف، سيتم أولاً تهيئة الجداول ثم تشغيل البوت
-if __name__ == '__main__':
-    run_bot()
-
-# ملاحظة: يمكنك استخدام هذا الملف لتشغيل خادم Flask (إذا كنت تخطط لذلك)
-# بدلاً من run_bot() إذا كنت تنشر كخدمة ويب
